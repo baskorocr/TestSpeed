61314f4040160a9f57f47fac83f8d239ebd95412
DESKTOP-JINUVIU\Bas-PC <mahesa.23@students.amikom.ac.id>
2022-04-14 14:38:26 +0700

    no message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -96,8 +96,8 @@
        <div class="col-lg-6">
           <div class="main">
              <h3><a>Kirim Ulasan mu, sekarang juga!</a></h3>
-             <form role="form" action="{{route('post.store')}}" method="post">
-              {{csrf_field}}
+             <form role="form" action="{{url('post-store')}}" method="post">
+                @csrf
                 <div class="form-group">
                    <label for="title">Post Title<span class="text-danger">*</span></label>
                    <input type="text" name="title" class="form-control" required>
